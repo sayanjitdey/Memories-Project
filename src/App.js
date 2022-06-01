e1fa023d1cefa027682ef9c2f1fb890aa4503016
sayanjitdey <dey.sayanjit1996@gmail.com>
2022-06-01 11:44:38 +0530

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import memories from './images/memories.png';
+import Posts from './Components/Posts/Posts';
+import Form from './Components/Form/Form';
+import useStyles from './styles';
+import { useDispatch } from 'react-redux';
+import { getPosts } from './actions/posts';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts());
+
+    }, [dispatch]);
+
+    return (
+
+        <Container maxwidth="lg">
+            <AppBar position='static' color='inherit' className={classes.appBar}>
+                <Typography variant='h2' align='center' className={classes.heading}>Memories</Typography>
+                <img src={memories} alt="memories" />
+            </AppBar>
+            <Grow in>
+                <Grid container justifyContent='space-between' alignItems='stretch' spacing={3}>
+                    <Grid item xs={12} sm={7}>
+                        <Posts />
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <Form />
+                    </Grid>
+                </Grid>
+            </Grow>
+        </Container>
+    );
 }
 
 export default App;
