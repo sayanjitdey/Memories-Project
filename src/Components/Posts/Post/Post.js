41d87bc05e76c7d2fd8fabd232b74ad06d8f40d8
sayanjitdey <dey.sayanjit1996@gmail.com>
2022-06-01 13:11:17 +0530

    updated

--- src/Components/Posts/Post/Post.js
+++ src/Components/Posts/Post/Post.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import useStyles from './styles';
-
+import { Card, CardAction, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+// import ThumbUpAltIcon from '@material-ui/icon/ThumbUpAl';
 const Post = () => {
     const classes = useStyles();
 
--- src/Components/Posts/Posts.js
+++ src/Components/Posts/Posts.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import Post from './Post/Post'
 import useStyles from './styles';
+import { Grid, CircularProgress } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 const Posts = () => {
     const classes = useStyles();
     const posts = useSelector((state) => state.posts);
     console.log(posts);
     return (
-        <>
-            <h1>Posts</h1>
-            <Post />
-            <Post />
-        </>
+        !posts.length ? <CircularProgress /> : (
+            <Grid className={classes.container} conintaner alignItems='stretch' spacing={3}>
+                {posts.map((post) => (
+                    <Grid key={post.id} item xs={12} sm={6}>
+                        <Post post={post} />
+                    </Grid>
+                ))}
+
+            </Grid>
+        )
     )
 };
 
